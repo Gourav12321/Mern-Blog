681d5cef90cc481a50f4bd5699930e648013010c
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-08-24 10:55:49 +0530

    se

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/api/posts', postRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000; // Use process.env.PORT or default to 5000
+const PORT = process.env.PORT || 5001; // Use process.env.PORT or default to 5000
 
 
 
@@ -52,7 +52,6 @@ const PORT = process.env.PORT || 5000; // Use process.env.PORT or default to 500
 //   .catch(error => {
 //     console.error('Failed to connect to MongoDB:', error);
 //   });
-
 app.listen(PORT,()=>{
   console.log(`server running port no : ${PORT}`)
 })
